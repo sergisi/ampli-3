b3491bb347a661ec4ab366aab530f4029e9f4d7d
Sergi Simon Balcells <sergisiba@gmail.com>
2020-04-27 18:26:59 +0200

    Added skeleton to ContainerTest for both Containers, so we can keep it simple to divide tasks

---
+++ tests/complex/ContainerTest.java
@@ -0,0 +1,26 @@
+package complex;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContainerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void registerConstant() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void registerFactory() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void registerSingleton() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getObject() {
+    }
+}
