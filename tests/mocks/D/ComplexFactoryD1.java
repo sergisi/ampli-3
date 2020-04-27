ec4907450172c007765dc7ca565bbe1a02280ace
Sergi Simon Balcells <sergisiba@gmail.com>
2020-04-27 18:59:33 +0200

    Added B. Changed D
    It was later reread the implementation given by the professor, so it was used his instead as a model. as it was clearer.

--- tests/mocks/D/ComplexFactoryD1.java
+++ tests/mocks/D/ComplexFactoryD1.java
@@ -5,9 +5,12 @@ import common.DependencyException;
 public class ComplexFactoryD1 implements complex.Factory<InterfaceD> {
     @Override
     public InterfaceD create(Object... parameters) throws DependencyException {
-        if (parameters.length == 0) {
-            throw new DependencyException("Needs at leat one parameter. More will be discarded");
+        int i;
+        try {
+            i = (int) parameters[0];
+        } catch(ClassCastException | ArrayIndexOutOfBoundsException ex) {
+            throw new DependencyException(ex);
         }
-        return new ImplementationD1((int) parameters[0]);
+        return new ImplementationD1(i);
     }
 }
--- tests/mocks/D/SimpleFactoryD1.java
+++ tests/mocks/D/SimpleFactoryD1.java
@@ -4,10 +4,13 @@ import common.DependencyException;
 
 public class SimpleFactoryD1 implements simple.Factory {
     @Override
-    public Object create(Object... parameters) throws DependencyException {
-        if (parameters.length == 0) {
-            throw new DependencyException("Needs at leat one parameter. More will be discarded");
+    public InterfaceD create(Object... parameters) throws DependencyException {
+        int i;
+        try {
+            i = (int) parameters[0];
+        } catch(ClassCastException | ArrayIndexOutOfBoundsException ex) {
+            throw new DependencyException(ex);
         }
-        return new ImplementationD1((int) parameters[0]);
+        return new ImplementationD1(i);
     }
 }
